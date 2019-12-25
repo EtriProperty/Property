a492922129d976829f6525293e2c695a7a24cf6b
Opdata <pb03641@gmail.com>
2019-12-26 00:34:06 +0900

    [feat] 매물찾기 매물등록 기본 API추가

--- back/routes/index.js
+++ back/routes/index.js
@@ -2,18 +2,45 @@ const express = require("express");
 const router = express.Router();
 const userModel = require("../models").userinfo;
 
+//매물찾기
+router.get("/properties", (req, res) => {
+  res.render("properties.html");
+});
+
+//매물등록
+router.get("/properties-register", (req, res) => {
+  if (!req.session.logined) {
+    //비로그인일떄
+    res.redirect("/");
+  } else {
+    res.render("properties-register.html");
+  }
+});
+
+//마이페이지
+router.get("/membershipInfoRe", (req, res) => {
+  if (!req.session.logined) {
+    //비로그인일떄
+    res.render("index.html");
+  } else {
+    res.render("membershipInfoRe.html");
+  }
+});
+
+// 로그아웃 API
 router.get("/logout", (req, res) => {
   req.session.destroy();
   res.render("index.html");
 });
-// GET home page.
-router.get("/", async (req, res, next) => {
+
+// 페이지로드 API
+router.get("/", async (req, res) => {
   if (!req.session.logined) {
     //비로그인일떄
     res.render("index.html");
   } else {
     //이미 로그인 상태일떄
-    res.render("login_on_index.html");
+    res.render("index.html"); //비로그인페이지로수정해야함
   }
 });
 
