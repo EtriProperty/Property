2e652c0feea8d57a9e13cc21978dff979f02e0ed
Opdata <pb03641@gmail.com>
2019-12-26 03:22:55 +0900

    [feat] 세션확인하여 로그인 유무에따라 다른 페이지 로드, 로그아웃 API 추가

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,21 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const userModel = require("../models").userinfo;
-
-//매물찾기
-router.get("/properties", (req, res) => {
-  res.render("properties.html");
-});
-
-//매물등록
-router.get("/properties-register", (req, res) => {
-  if (!req.session.logined) {
-    //비로그인일떄
-    res.redirect("/");
-  } else {
-    res.render("properties-register.html");
-  }
-});
 
 //마이페이지
 router.get("/membershipInfoRe", (req, res) => {
