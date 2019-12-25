c7e5a2f8354281962580598b7408b0fd7a97f77b
Opdata <pb03641@gmail.com>
2019-12-26 03:24:11 +0900

    [feat] 세션 검사 추가

--- back/routes/mywishlist.js
+++ back/routes/mywishlist.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express();
 
-router.get("/userInfoUpdate", function(req, res) {
-  //   res.render("index.html"); 경로 맞음
-});
 /*
-router.get("/", verifytoken, (req, res) => {
-  res.json(req.decoded);
-  //   res.render("myWishList.html");
+router.get("/userInfoUpdate", function(req, res) {
+     res.render("index.html"); 경로 맞음
 });
 */
 
 router.get("/", (req, res) => {
-  res.render("myWishList.html");
+  if (!req.session.logined) {
+    res.redirect("/");
+  } else {
+    res.render("myWishList.html");
+  }
 });
 
 module.exports = router;
