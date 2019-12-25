7d38b71c52a0398d0a8a9b35e824d9da959ea2b3
Opdata <pb03641@gmail.com>
2019-12-25 22:45:52 +0900

    [feat] 로그인 API 추가

--- back/routes/login.js
+++ back/routes/login.js
@@ -1,42 +1,45 @@
+const express = require("express");
+const router = express();
+const userinfo = require("../models").userinfo;
+const session = require("express-session");
+const crypto = require("crypto");
+
 //로그인 기능만(입력받은 비밀번호와 db의 비밀번호와 비교 후 넘겨줌)
-var express = require("express");
-var login = require("../models").userinfo;
-//var crypto = require("crypto");
-
-var router = express.Router();
-
-/* GET login page. */
-router.get("/", function(req, res) {
-  res.render("signIn.html");
-});
-
-/* 테스트 안했음
 router.post("/login_on", async function(req, res, next) {
-  let body = req.body;
-  let result = await login.findOne({
+  const body1 = req.body;
+  const id = body1.userid;
+
+  let result = await userinfo.findOne({
     where: {
-      email: body.email
+      id: id
     }
   });
+
   let dbPassword = result.dataValues.password;
-  let inputPassword = body.password;
   let salt = result.dataValues.salt;
+  let inputPassword = body1.password;
   let hashPassword = crypto
     .createHash("sha512")
     .update(inputPassword + salt)
     .digest("hex");
 
   if (dbPassword === hashPassword) {
-    console.log("비밀번호 일치");
-    res.json({
-      message: "userId",
-      result_code: 200
-    });
-    res.redirect("/index");
+    req.session.logined = true;
+    req.session.userid = body1.userid;
+    res.redirect("index");
   } else {
-    console.log("비밀번호 불일치");
-    res.redirect("/index");
+    // 비밀번호불일치
+    res.redirect("index");
   }
 });
-*/
+
+router.get("/index", function(req, res) {
+  res.render("index.html");
+});
+
+/* GET login page. */
+router.get("/", function(req, res) {
+  res.render("signIn.html");
+});
+
 module.exports = router;
